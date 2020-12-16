990e99dd3a02d3e228dfc9e1669ee702b44207c2
zahidmughal <zahisohna@gmail.com>
2020-12-16 06:40:49 -0800

    call function on click button to change html

--- js.js
+++ js.js
@@ -2,7 +2,7 @@
 //     // document.getElementById("para").innerHTML="<h1>through js this para </h1>";
    
 // };
-window.onload=check1;
+// window.onload=check1;
 function check1(){
     var a = document.getElementById("para");
     a.innerHTML="<h1>through js this para </h1>";
