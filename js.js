7af73dcaa2c604a57b972840b6048e55d934cd69
zahidmughal <zahisohna@gmail.com>
2020-12-16 09:06:20 -0800

    build function for calculator using if else

--- js.js
+++ js.js
@@ -8,11 +8,39 @@
 //     a.innerHTML="<h1>through js this para </h1>";
 // }
 
-function check1(){
-document.getElementById("para").innerText="thrugh js";
+// function check1(){
+// document.getElementById("para").innerText="thrugh js";
 // h.innerHTML="thrugh js";
 // document.getElementById("para").style.color="green";
 // document.getElementById("para").style.fontSize="50px";
 // document.getElementsByTagName
 
-}
+// }
+// document.write(eval(5+9));
+function calculate() {
+    var x = parseInt(document.getElementById("1st-num").value);
+    var y = parseInt(document.getElementById("2nd-num").value);
+    var o = document.getElementById("op").value;
+    //var sum = x+y;
+    if (o == "+") {
+        document.getElementById("result").value= x+y ;
+    }
+    else if (o == "-") {
+        document.getElementById("result").value= x-y ;
+    }
+    else if (o == "*") {
+        document.getElementById("result").value= x*y ;
+    }
+    else if (o == "/") {
+        document.getElementById("result").value= x/y ;
+    }
+    alert(x);
+   
+    
+    
+    
+    
+    
+    
+    
+    }
