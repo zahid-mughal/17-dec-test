1da2f4552868543b166bc8cc0b43b790e7aff935
zahidmughal <zahisohna@gmail.com>
2020-12-16 06:29:56 -0800

    window.onload

---
+++ js.js
@@ -0,0 +1,4 @@
+window.onload=function(){
+    document.getElementById("para").innerHTML="<h1>through js this para </h1>";
+};
+// document.getElementById("para").innerHTML="<h1>through js this para </h1>";
