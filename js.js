e2ebab57033914b100621a62f1f79ca27035bd3d
zahidmughal <zahisohna@gmail.com>
2020-12-16 06:38:16 -0800

    function call in window.onload

--- js.js
+++ js.js
@@ -1,4 +1,9 @@
-window.onload=function(){
-    document.getElementById("para").innerHTML="<h1>through js this para </h1>";
-};
-// document.getElementById("para").innerHTML="<h1>through js this para </h1>";
+// window.onload=function(){
+//     // document.getElementById("para").innerHTML="<h1>through js this para </h1>";
+   
+// };
+window.onload=check1;
+function check1(){
+    var a = document.getElementById("para");
+    a.innerHTML="<h1>through js this para </h1>";
+}
